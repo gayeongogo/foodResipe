79437fcdd8a655b10cb773f59b2869d879048661
gayeongogo <rkdus1357@gmail.com>
2023-05-01 14:54:48 +0900

    meal details modal

--- script.js
+++ script.js
@@ -6,9 +6,16 @@
 
 const searchBtn = document.getElementById('search-btn');
 const mealList = document.getElementById('meal');
+const mealDetailsContent = document.querySelector('.meal-details-content')
+const recipeCloseBtn = document.getElementById('recipe-close-btn')
 
 
 searchBtn.addEventListener('click', getMealList);
+mealList.addEventListener('click', getMealRecipe);
+recipeCloseBtn.addEventListener('click', () => {
+    mealDetailsContent.parentElement.classList.remove('showRecipe');
+})
+
 
 function getMealList() {
     let serchInputText = document.getElementById('search-input').value.trim();//trim(): 앞뒤 공백을 지워줌 -> 공백 있어도 검색해줌
@@ -44,4 +51,38 @@ function getMealList() {
     .catch(error => {
         console.log(error)
     })
+}
+
+function getMealRecipe(e) {
+    e.preventDefault();
+    //console.log(e.target);
+    if(e.target.classList.contains('recipe-btn')) {
+        let mealItem = e.target.parentElement.parentElement;
+        //console.log(mealItem);
+        fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${mealItem.dataset.id}`)
+        .then(response => response.json())
+        .then(data => {mealRecipeModal(data.meals)})
+    }
+}
+
+function mealRecipeModal(meal) {
+    //console.log(meal);
+    meal = meal[0];
+    let html = `
+        <h2 class="recipe-title">${meal.strMeal}</h2>
+        <p class="recipe-category">${meal.strCategory}</p>
+        <div class="recipe-instruct">
+            <h3>Instructions:</h3>
+            <p>${meal.strInstructions}</p>
+        </div>
+        <div class="recipe-meal-img">
+            <img src="${meal.strMealThumb}" alt="food">
+        </div>
+        <div class="recipe-link">
+            <a href="${meal.strYoutube}" target="_blank">Watch Video</a>
+        </div>
+    `;
+
+    mealDetailsContent.innerHTML = html;
+    mealDetailsContent.parentElement.classList.add('showRecipe');
 }
